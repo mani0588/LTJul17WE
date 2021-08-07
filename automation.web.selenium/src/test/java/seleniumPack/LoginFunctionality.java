a9e937692c808dad78ee2da90bb0af33bfe9e398
Manikandan <55494815+mani0588@users.noreply.github.com>
2021-08-07 21:40:25 +0530

    Update LoginFunctionality.java

--- automation.web.selenium/src/test/java/seleniumPack/LoginFunctionality.java
+++ automation.web.selenium/src/test/java/seleniumPack/LoginFunctionality.java
@@ -1,39 +1,44 @@
 package seleniumPack;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import seleniumPack.LoginPage;
 
 public class LoginFunctionality {
 
 	WebDriver driver;
-	
-	// Test types - Regression, etc
-	// Browser compatability
-	// testng xml
-	// paramters
-	// screenshots
+
 	// logs
 	// custom asserts
 
 	@Test // test case
-	public void verifyLoginForInvalidCredentials() {
+	public void verifyLoginForInvalidCredentials() throws IOException {
 		String un = "neeraja";
 		String pwd = "neeraja";
-		
+
 		LoginPage loginPage = new LoginPage(driver);
-		String errorText = loginPage.login(un, pwd).login(un, pwd).login(un, pwd)
-				.getLoginErrorMsg();
+		String errorText = loginPage.login(un, pwd).login(un, pwd).login(un, pwd).getLoginErrorMsg();
 		Assert.assertEquals(errorText, "Incorrect login, please try again.");
+
+		File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(screen, new File(
+				"C:\\Users\\p7165950\\Documents\\automation\\LTJul17WE\\automation.web.selenium\\MyContactScreen.png"));
+
 	}
 	
 	@Test(dataProvider="getLoginData")
